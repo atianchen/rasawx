4eaab027c8d49f01f8e36d123d5eb7a52e094218
yonyoujensenchen <atianchen@gmail.com>
2019-07-30 22:24:11 +0800

    add wx response

--- wx/message.py
+++ wx/message.py
@@ -25,13 +25,13 @@ class WxMsg():
         self.createTime = root.findtext("CreateTime")
         self.content = root.findtext("Content")
 
-<<<<<<< HEAD:wx/msg/WxMsg.py
+
     def toXML(self):
         root = ElementTree.Element("xml")
         toUserNameNode = ElementTree.SubElement(root,'ToUserName')
-        toUserNameNode.text = self.toUserName
+        toUserNameNode.text = ElementTree.CDATA(self.toUserName)
         fromUserNameNode = ElementTree.SubElement(root,"FromUserName")
-        fromUserNameNode.text = self.fromUserName
+        fromUserNameNode.text = ElementTree.CDATA(self.fromUserName)
         contentNode = ElementTree.SubElement(root,"Content")
         contentNode.text = ElementTree.CDATA(self.content)
         if not self.msgId.isspace():
@@ -44,23 +44,6 @@ class WxMsg():
         return ElementTree.dump(root)
 
 
-
-=======
-    def toXml(self) -> str:
-        return None
->>>>>>> 466204a3b7374169e518f64788c0f280f82cb1ef:wx/message.py
-
     def __str__(self):
         return "%s %s %s " % (self.msgId, self.msgType,self.fromUserName)
 
-xml = '''<xml>
-      <ToUserName><![CDATA[toUser]]></ToUserName>
-      <FromUserName><![CDATA[fromUser]]></FromUserName>
-      <CreateTime>1348831860</CreateTime>
-      <MsgType><![CDATA[text]]></MsgType>
-      <Content><![CDATA[this is a test]]></Content>
-      <MsgId>1234567890123456</MsgId>
-    </xml>'''
-msg = WxMsg(xml)
-print(msg.__str__())
-print(msg.toXML())
